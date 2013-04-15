39e33195739283b2d120726d7b0cc3e8f3228351
Sam DeLuca <sam@decarboxy.com>
2013-04-15 10:42:35 -0500

    updating configure_rosetta_repo.sh

--- configure_rosetta_repo.sh
+++ configure_rosetta_repo.sh
@@ -11,7 +11,8 @@
 # Brief:   This shell script clones repositories from GitHub and configures   #
 #          them to play nicely with how our community is organized.           #
 #                                                                             #
-# Author:  Brian D. Weitzner (brian.weitzner@gmail.com)                       #
+# Authors:  Brian D. Weitzner (brian.weitzner@gmail.com)                      #
+#           Tim Jacobs (TimJacobs2@gmail.com)                                 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 if [ -z "$1" ]; then
@@ -27,7 +28,7 @@ github_user_name=$1
 github_password=$2
 repo="rosetta"
 
-read -p "Where do you to clone $repo? " path
+read -p "Where would you like to clone $repo? " path
 if [ -z $path ]; then
 	path="."
 fi
@@ -37,18 +38,27 @@ if [ ! -d $path ]; then
 	while true; do
 		read -p "Would you like to create this directory now? " yn
 		case $yn in
-			[Yy]* ) mkdir $path; break;;
-			[Nn]* ) exit;;
-			* ) echo "Please answer yes or no.";;
+			[Yy] | [Yy][Ee][Ss] ) mkdir $path; break;;
+			[Nn] | [Nn][Oo] ) exit;;
+		* ) echo "Please answer yes (y) or no (n).";;
 		esac
 	done
 fi  
 
+while true; do
+	read -p "Would you like to clone over ssh (s) or https (h) - Note that ssh keys are required for cloning over ssh (Default: ssh)? " protocol
+	case $protocol in
+		[Ss] | [Ss][Ss][Hh] | "" ) url=git@github.com:RosettaCommons/; break;;
+		[Hh] | [Hh][Tt][Tt][Pp][Ss] ) url=https://$github_user_name:$github_password@github.com/RosettaCommons/; break;;
+	*) echo "Please answer ssh (s) or https (h).";;
+	esac
+done
+
 path="$path/"
 
 echo "\033[0;34mCloning Rosetta...\033[0m"
-hash git >/dev/null && /usr/bin/env git clone git@github.com:RosettaCommons/$repo.git $path$repo || {
-echo "git is not installed!"
+hash git >/dev/null && /usr/bin/env git clone $url$repo.git $path$repo || {
+echo "Can't clone! It's likely that git is not installed and/or you are cloning over SSH without ssh keys setup.\nSee https://help.github.com/articles/error-permission-denied-publickey for instructions on how to setup SSH keys for github."
 exit
 }
 								
@@ -84,8 +94,9 @@ for hook in pre-commit post-commit; do
 done
 
 echo "\033[0;34mConfiguring aliases...\033[0m"
-git config alias.tracked-branch "\!sh -c 'git checkout -b \$1 && git push origin \$1:\$2/\$1 && git branch --set-upstream \$1  origin/\$2/\$1' -"
-git config alias.personal-tracked-branch "\!sh -c 'git tracked-branch \$1 $github_user_name' -"
+git config alias.tracked-branch '!sh -c "git checkout -b $1 && git push origin $1:$2/$1 && git branch --set-upstream $1  origin/$2/$1" -'
+git config alias.personal-tracked-branch '!sh -c "git tracked-branch $1 $github_user_name" -'
+sed -ie "s/\$github_user_name/$github_user_name/g" .git/config
 
 git config --global alias.show-graph "log --graph --abbrev-commit --pretty=oneline"
 
@@ -94,7 +105,7 @@ git config --global color.branch auto
 git config --global color.diff auto
 git config --global color.interactive auto
 git config --global color.status auto
-	
+
 cd $starting_dir
 echo "\033[0;32mDone configuring your Rosetta git repository!\033[0m"
 echo "\033[0;32mRemember to check out rosetta_tools and rosetta_demos repositories!\033[0m"
