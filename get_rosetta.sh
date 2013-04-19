682791e3bc4beffd9f9f5fd12cc98851ce16f8d3
Sam DeLuca <sam@decarboxy.com>
2013-04-19 14:26:07 -0500

    chaning alias color-echo to variable color_echo

--- get_rosetta.sh
+++ get_rosetta.sh
@@ -38,9 +38,9 @@ fi
 
 # Ensure the output is colorized to make it a little easier to read
 os_type=`uname`
-alias color-echo="echo"
+color_echo="echo"
 if [ $os_type == "Linux" ]; then
-    alias color-echo="echo -e"
+    color_echo="echo -e"
 fi
 
 # Prevent git from using a graphical password prompt
@@ -48,17 +48,17 @@ unset SSH_ASKPASS
 
 main()
 {
-    color-echo  "\033[0;32mConfiguring the Rosetta GitHub repository on your machine.\033[0m"
-    color-echo  "\033[0;34mMake sure you have already\033[0m"
-    color-echo  "\033[0;34m   1) created your GitHub account\033[0m"
-    color-echo  "\033[0;34m   2) emailed your GitHub user name to Andrew Leaver-Fay (aleaverfay@gmail.com)\033[0m"
-    color-echo  "\033[0;34m   3) Set up SSH keys to GitHub on your machine following the instructions here:\033[0m"
-    color-echo  "\033[0;34m      https://help.github.com/articles/generating-ssh-keys\033[0m"
-    color-echo  "\033[0;34m   4) to use HTTPS, follow the instructions for password caching here:\033[0m"
-    color-echo  "\033[0;34m      https://help.github.com/articles/set-up-git\033[0m"
-    color-echo 
+    $color_echo  "\033[0;32mConfiguring the Rosetta GitHub repository on your machine.\033[0m"
+    $color_echo  "\033[0;34mMake sure you have already\033[0m"
+    $color_echo  "\033[0;34m   1) created your GitHub account\033[0m"
+    $color_echo  "\033[0;34m   2) emailed your GitHub user name to Andrew Leaver-Fay (aleaverfay@gmail.com)\033[0m"
+    $color_echo  "\033[0;34m   3) Set up SSH keys to GitHub on your machine following the instructions here:\033[0m"
+    $color_echo  "\033[0;34m      https://help.github.com/articles/generating-ssh-keys\033[0m"
+    $color_echo  "\033[0;34m   4) to use HTTPS, follow the instructions for password caching here:\033[0m"
+    $color_echo  "\033[0;34m      https://help.github.com/articles/set-up-git\033[0m"
+    $color_echo 
     read -p "Please enter your GitHub username: " github_user_name
-    color-echo  "\n"
+    $color_echo  "\n"
 
     read -p "Where would you like to clone Rosetta? " path
     if [ -z $path ]; then
@@ -66,13 +66,13 @@ main()
     fi
 
     if [ ! -d $path ]; then
-        color-echo  "\033[0;33m'$path' does not exist!\033[0m You'll need to create '$path' if you want to install rosetta there."
+        $color_echo  "\033[0;33m'$path' does not exist!\033[0m You'll need to create '$path' if you want to install rosetta there."
         while true; do
             read -p "Would you like to create this directory now [y/n]? " yn
             case $yn in
                 [Yy] | [Yy][Ee][Ss] ) mkdir $path; break;;
                 [Nn] | [Nn][Oo] ) exit;;
-            * ) color-echo  "Please answer yes (y) or no (n).";;
+            * ) $color_echo  "Please answer yes (y) or no (n).";;
             esac
         done
     fi  
@@ -82,7 +82,7 @@ main()
         case $protocol in
             [Ss] | [Ss][Ss][Hh] | "" ) url=git@github.com:RosettaCommons/; break;;
             [Hh] | [Hh][Tt][Tt][Pp][Ss] ) url=https://$github_user_name@github.com/RosettaCommons/; break;;
-        *) color-echo  "Please answer SSH (s) or HTTPS (h).";;
+        *) $color_echo  "Please answer SSH (s) or HTTPS (h).";;
 		esac	
     done
 	
@@ -91,7 +91,7 @@ main()
         case $yn in
             [Yy] | [Yy][Ee][Ss] ) parallel=true; break;;
             [Nn] | [Nn][Oo] ) parallel=false; break;;
-        * ) color-echo  "Please answer yes (y) or no (n).";;
+        * ) $color_echo  "Please answer yes (y) or no (n).";;
         esac
     done
 
@@ -100,7 +100,7 @@ main()
     fi
     path="$path/Rosetta/"
 
-    color-echo  "\033[0;34mConfiguring...\033[0m"
+    $color_echo  "\033[0;34mConfiguring...\033[0m"
     print_repo Super
 
     starting_dir=$PWD
@@ -121,45 +121,45 @@ main()
 	fi
     
     wait
-    color-echo  "\033[0;32mDone configuring your Rosetta git repository!\033[0m"
+    $color_echo  "\033[0;32mDone configuring your Rosetta git repository!\033[0m"
 }
 
 configure_repo()
 {
     hash git >/dev/null && /usr/bin/env git clone $url$1.git $path$1 || {
-        color-echo  "Can't clone! It's likely that git is not installed and/or you are cloning over SSH without SSH keys setup."
-        color-echo  "See https://help.github.com/articles/error-permission-denied-publickey for instructions on how to setup SSH keys for GitHub."
+        $color_echo  "Can't clone! It's likely that git is not installed and/or you are cloning over SSH without SSH keys setup."
+        $color_echo  "See https://help.github.com/articles/error-permission-denied-publickey for instructions on how to setup SSH keys for GitHub."
         exit
     }
 
     print_repo $1
-    color-echo  "\n\n \033[0;32m....is now cloned.\033[0m"
+    $color_echo  "\n\n \033[0;32m....is now cloned.\033[0m"
 
     cd $path/$1
 
-    color-echo  "\033[0;34mDisabling fast-forward merges on master...\033[0m"
+    $color_echo  "\033[0;34mDisabling fast-forward merges on master...\033[0m"
     git config branch.master.mergeoptions "--no-ff"
 
-    color-echo  "\033[0;34mConfiguring commit message template...\033[0m"
+    $color_echo  "\033[0;34mConfiguring commit message template...\033[0m"
     git config commit.template .commit_template.txt
 
     cd .git/hooks
     for hook in "${hooks[@]}"; do 
-        color-echo  "\033[0;34mConfiguring the $hook hook...\033[0m"
+        $color_echo  "\033[0;34mConfiguring the $hook hook...\033[0m"
         curl -L $hook_url/$hook > $hook
         chmod +x $hook
     done
 
     cd ../..
 
-    color-echo  "\033[0;34mConfiguring aliases...\033[0m"
+    $color_echo  "\033[0;34mConfiguring aliases...\033[0m"
     git config alias.tracked-branch '!sh -c "git checkout -b $1 && git push origin $1:$2/$1 && git branch --set-upstream $1  origin/$2/$1" -'
     git config alias.personal-tracked-branch '!sh -c "git tracked-branch $1 $github_user_name" -'
     sed -ie "s/\$github_user_name/$github_user_name/g" .git/config
 
     git config alias.show-graph "log --graph --abbrev-commit --pretty=oneline"
 
-    color-echo  "\033[0;34mConfiguring git colors...\033[0m"
+    $color_echo  "\033[0;34mConfiguring git colors...\033[0m"
     git config color.branch auto
     git config color.diff auto
     git config color.interactive auto
@@ -169,60 +169,60 @@ configure_repo()
 print_repo() 
 {
     if [ $1 == "Super" ]; then
-        color-echo  "\033[0;32m"'     ___           ___           ___           ___           __         ___         ___      '"\033[0m"
-        color-echo  "\033[0;32m"'    /\  \         /\  \         /\__\         /\__\         /\__\      /\__\       /\  \     '"\033[0m"
-        color-echo  "\033[0;32m"'   /::\  \       /::\  \       /:/ _/_       /:/ _/_       /:/  /     /:/  /      /::\  \    '"\033[0m"
-        color-echo  "\033[0;32m"'  /:/\:\__\     /:/\:\  \     /:/ /\  \     /:/ /\__\     /:/__/     /:/__/      /:/\:\  \   '"\033[0m"
-        color-echo  "\033[0;32m"' /:/ /:/  /    /:/  \:\  \   /:/ /::\  \   /:/ /:/ _/_   /::\  \    /::\  \     /:/ /::\  \  '"\033[0m"
-        color-echo  "\033[0;32m"'/:/_/:/__/___ /:/__/ \:\__\ /:/_/:/\:\__\ /:/_/:/ /\__\ /:/\:\  \  /:/\:\  \   /:/_/:/\:\__\ '"\033[0m"
-        color-echo  "\033[0;32m"'\:\/:::::/  / \:\  \ /:/  / \:\/:/ /:/  / \:\/:/ /:/  / \/__\:\  \ \/__\:\  \  \:\/:/  \/__/ '"\033[0m"
-        color-echo  "\033[0;32m"' \::/~~/~~~~   \:\  /:/  /   \::/ /:/  /   \::/_/:/  /       \:\  \     \:\  \  \::/__/      '"\033[0m"
-        color-echo  "\033[0;32m"'  \:\~~\        \:\/:/  /     \/_/:/  /     \:\/:/  /         \:\  \     \:\  \  \:\  \      '"\033[0m"
-        color-echo  "\033[0;32m"'   \:\__\        \::/  /        /:/  /       \::/  /           \:\__\     \:\__\  \:\__\     '"\033[0m"
-        color-echo  "\033[0;32m"'    \/__/         \/__/         \/__/         \/__/             \/__/      \/__/   \/__/     '"\033[0m"
+        $color_echo  "\033[0;32m"'     ___           ___           ___           ___           __         ___         ___      '"\033[0m"
+        $color_echo  "\033[0;32m"'    /\  \         /\  \         /\__\         /\__\         /\__\      /\__\       /\  \     '"\033[0m"
+        $color_echo  "\033[0;32m"'   /::\  \       /::\  \       /:/ _/_       /:/ _/_       /:/  /     /:/  /      /::\  \    '"\033[0m"
+        $color_echo  "\033[0;32m"'  /:/\:\__\     /:/\:\  \     /:/ /\  \     /:/ /\__\     /:/__/     /:/__/      /:/\:\  \   '"\033[0m"
+        $color_echo  "\033[0;32m"' /:/ /:/  /    /:/  \:\  \   /:/ /::\  \   /:/ /:/ _/_   /::\  \    /::\  \     /:/ /::\  \  '"\033[0m"
+        $color_echo  "\033[0;32m"'/:/_/:/__/___ /:/__/ \:\__\ /:/_/:/\:\__\ /:/_/:/ /\__\ /:/\:\  \  /:/\:\  \   /:/_/:/\:\__\ '"\033[0m"
+        $color_echo  "\033[0;32m"'\:\/:::::/  / \:\  \ /:/  / \:\/:/ /:/  / \:\/:/ /:/  / \/__\:\  \ \/__\:\  \  \:\/:/  \/__/ '"\033[0m"
+        $color_echo  "\033[0;32m"' \::/~~/~~~~   \:\  /:/  /   \::/ /:/  /   \::/_/:/  /       \:\  \     \:\  \  \::/__/      '"\033[0m"
+        $color_echo  "\033[0;32m"'  \:\~~\        \:\/:/  /     \/_/:/  /     \:\/:/  /         \:\  \     \:\  \  \:\  \      '"\033[0m"
+        $color_echo  "\033[0;32m"'   \:\__\        \::/  /        /:/  /       \::/  /           \:\__\     \:\__\  \:\__\     '"\033[0m"
+        $color_echo  "\033[0;32m"'    \/__/         \/__/         \/__/         \/__/             \/__/      \/__/   \/__/     '"\033[0m"
 
     elif [ $1 == "rosetta" ]; then
-        color-echo  "\033[0;32m"'      ___           ___                       ___      '"\033[0m"
-        color-echo  "\033[0;32m"'     /\  \         /\  \                     /\  \     '"\033[0m"
-        color-echo  "\033[0;32m"'    |::\  \       /::\  \       ___          \:\  \    '"\033[0m"
-        color-echo  "\033[0;32m"'    |:|:\  \     /:/\:\  \     /\__\          \:\  \   '"\033[0m"
-        color-echo  "\033[0;32m"'  __|:|\:\  \   /:/ /::\  \   /:/__/      _____\:\  \  '"\033[0m"
-        color-echo  "\033[0;32m"' /::::|_\:\__\ /:/_/:/\:\__\ /::\  \     /::::::::\__\ '"\033[0m"
-        color-echo  "\033[0;32m"' \:\~~\  \/__/ \:\/:/  \/__/ \/\:\  \__  \:\~~\~~\/__/ '"\033[0m"
-        color-echo  "\033[0;32m"'  \:\  \        \::/__/       ~~\:\/\__\  \:\  \       '"\033[0m"
-        color-echo  "\033[0;32m"'   \:\  \        \:\  \          \::/  /   \:\  \      '"\033[0m"
-        color-echo  "\033[0;32m"'    \:\__\        \:\__\         /:/  /     \:\__\     '"\033[0m"
-        color-echo  "\033[0;32m"'     \/__/         \/__/         \/__/       \/__/     '"\033[0m"
+        $color_echo  "\033[0;32m"'      ___           ___                       ___      '"\033[0m"
+        $color_echo  "\033[0;32m"'     /\  \         /\  \                     /\  \     '"\033[0m"
+        $color_echo  "\033[0;32m"'    |::\  \       /::\  \       ___          \:\  \    '"\033[0m"
+        $color_echo  "\033[0;32m"'    |:|:\  \     /:/\:\  \     /\__\          \:\  \   '"\033[0m"
+        $color_echo  "\033[0;32m"'  __|:|\:\  \   /:/ /::\  \   /:/__/      _____\:\  \  '"\033[0m"
+        $color_echo  "\033[0;32m"' /::::|_\:\__\ /:/_/:/\:\__\ /::\  \     /::::::::\__\ '"\033[0m"
+        $color_echo  "\033[0;32m"' \:\~~\  \/__/ \:\/:/  \/__/ \/\:\  \__  \:\~~\~~\/__/ '"\033[0m"
+        $color_echo  "\033[0;32m"'  \:\  \        \::/__/       ~~\:\/\__\  \:\  \       '"\033[0m"
+        $color_echo  "\033[0;32m"'   \:\  \        \:\  \          \::/  /   \:\  \      '"\033[0m"
+        $color_echo  "\033[0;32m"'    \:\__\        \:\__\         /:/  /     \:\__\     '"\033[0m"
+        $color_echo  "\033[0;32m"'     \/__/         \/__/         \/__/       \/__/     '"\033[0m"
 
     elif [ $1 == "rosetta_demos" ]; then
-        color-echo  "\033[0;32m"'                    ___           ___           ___           ___      '"\033[0m"
-        color-echo  "\033[0;32m"'     _____         /\__\         /\  \         /\  \         /\__\     '"\033[0m"
-        color-echo  "\033[0;32m"'    /::\  \       /:/ _/_       |::\  \       /::\  \       /:/ _/_    '"\033[0m"
-        color-echo  "\033[0;32m"'   /:/\:\  \     /:/ /\__\      |:|:\  \     /:/\:\  \     /:/ /\  \   '"\033[0m"
-        color-echo  "\033[0;32m"'  /:/  \:\__\   /:/ /:/ _/_   __|:|\:\  \   /:/  \:\  \   /:/ /::\  \  '"\033[0m"
-        color-echo  "\033[0;32m"' /:/__/ \:|__| /:/_/:/ /\__\ /::::|_\:\__\ /:/__/ \:\__\ /:/_/:/\:\__\ '"\033[0m"
-        color-echo  "\033[0;32m"' \:\  \ /:/  / \:\/:/ /:/  / \:\~~\  \/__/ \:\  \ /:/  / \:\/:/ /:/  / '"\033[0m"
-        color-echo  "\033[0;32m"'  \:\  /:/  /   \::/_/:/  /   \:\  \        \:\  /:/  /   \::/ /:/  /  '"\033[0m"
-        color-echo  "\033[0;32m"'   \:\/:/  /     \:\/:/  /     \:\  \        \:\/:/  /     \/_/:/  /   '"\033[0m"
-        color-echo  "\033[0;32m"'    \::/  /       \::/  /       \:\__\        \::/  /        /:/  /    '"\033[0m"
-        color-echo  "\033[0;32m"'     \/__/         \/__/         \/__/         \/__/         \/__/     '"\033[0m"
+        $color_echo  "\033[0;32m"'                    ___           ___           ___           ___      '"\033[0m"
+        $color_echo  "\033[0;32m"'     _____         /\__\         /\  \         /\  \         /\__\     '"\033[0m"
+        $color_echo  "\033[0;32m"'    /::\  \       /:/ _/_       |::\  \       /::\  \       /:/ _/_    '"\033[0m"
+        $color_echo  "\033[0;32m"'   /:/\:\  \     /:/ /\__\      |:|:\  \     /:/\:\  \     /:/ /\  \   '"\033[0m"
+        $color_echo  "\033[0;32m"'  /:/  \:\__\   /:/ /:/ _/_   __|:|\:\  \   /:/  \:\  \   /:/ /::\  \  '"\033[0m"
+        $color_echo  "\033[0;32m"' /:/__/ \:|__| /:/_/:/ /\__\ /::::|_\:\__\ /:/__/ \:\__\ /:/_/:/\:\__\ '"\033[0m"
+        $color_echo  "\033[0;32m"' \:\  \ /:/  / \:\/:/ /:/  / \:\~~\  \/__/ \:\  \ /:/  / \:\/:/ /:/  / '"\033[0m"
+        $color_echo  "\033[0;32m"'  \:\  /:/  /   \::/_/:/  /   \:\  \        \:\  /:/  /   \::/ /:/  /  '"\033[0m"
+        $color_echo  "\033[0;32m"'   \:\/:/  /     \:\/:/  /     \:\  \        \:\/:/  /     \/_/:/  /   '"\033[0m"
+        $color_echo  "\033[0;32m"'    \::/  /       \::/  /       \:\__\        \::/  /        /:/  /    '"\033[0m"
+        $color_echo  "\033[0;32m"'     \/__/         \/__/         \/__/         \/__/         \/__/     '"\033[0m"
         
     elif [ $1 == "rosetta_tools" ]; then
-        color-echo  "\033[0;32m"'      ___           ___           ___                           ___      '"\033[0m"
-        color-echo  "\033[0;32m"'     /\__\         /\  \         /\  \                         /\__\     '"\033[0m"
-        color-echo  "\033[0;32m"'    /:/  /        /::\  \       /::\  \     ___               /:/ _/_    '"\033[0m"
-        color-echo  "\033[0;32m"'   /:/__/        /:/\:\  \     /:/\:\  \   /\  \             /:/ /\  \   '"\033[0m"
-        color-echo  "\033[0;32m"'  /::\  \       /:/  \:\  \   /:/  \:\  \  \:\  \     ___   /:/ /::\  \  '"\033[0m"
-        color-echo  "\033[0;32m"' /:/\:\  \     /:/__/ \:\__\ /:/__/ \:\__\  \:\  \   /\__\ /:/_/:/\:\__\ '"\033[0m"
-        color-echo  "\033[0;32m"' \/__\:\  \    \:\  \ /:/  / \:\  \ /:/  /   \:\  \ /:/  / \:\/:/ /:/  / '"\033[0m"
-        color-echo  "\033[0;32m"'      \:\  \    \:\  /:/  /   \:\  /:/  /     \:\  /:/  /   \::/ /:/  /  '"\033[0m"
-        color-echo  "\033[0;32m"'       \:\  \    \:\/:/  /     \:\/:/  /       \:\/:/  /     \/_/:/  /   '"\033[0m"
-        color-echo  "\033[0;32m"'        \:\__\    \::/  /       \::/  /         \::/  /        /:/  /    '"\033[0m"
-        color-echo  "\033[0;32m"'         \/__/     \/__/         \/__/           \/__/         \/__/     '"\033[0m"
+        $color_echo  "\033[0;32m"'      ___           ___           ___                           ___      '"\033[0m"
+        $color_echo  "\033[0;32m"'     /\__\         /\  \         /\  \                         /\__\     '"\033[0m"
+        $color_echo  "\033[0;32m"'    /:/  /        /::\  \       /::\  \     ___               /:/ _/_    '"\033[0m"
+        $color_echo  "\033[0;32m"'   /:/__/        /:/\:\  \     /:/\:\  \   /\  \             /:/ /\  \   '"\033[0m"
+        $color_echo  "\033[0;32m"'  /::\  \       /:/  \:\  \   /:/  \:\  \  \:\  \     ___   /:/ /::\  \  '"\033[0m"
+        $color_echo  "\033[0;32m"' /:/\:\  \     /:/__/ \:\__\ /:/__/ \:\__\  \:\  \   /\__\ /:/_/:/\:\__\ '"\033[0m"
+        $color_echo  "\033[0;32m"' \/__\:\  \    \:\  \ /:/  / \:\  \ /:/  /   \:\  \ /:/  / \:\/:/ /:/  / '"\033[0m"
+        $color_echo  "\033[0;32m"'      \:\  \    \:\  /:/  /   \:\  /:/  /     \:\  /:/  /   \::/ /:/  /  '"\033[0m"
+        $color_echo  "\033[0;32m"'       \:\  \    \:\/:/  /     \:\/:/  /       \:\/:/  /     \/_/:/  /   '"\033[0m"
+        $color_echo  "\033[0;32m"'        \:\__\    \::/  /       \::/  /         \::/  /        /:/  /    '"\033[0m"
+        $color_echo  "\033[0;32m"'         \/__/     \/__/         \/__/           \/__/         \/__/     '"\033[0m"
     
     fi
 
-    color-echo 
+    $color_echo 
 }
 
 main
